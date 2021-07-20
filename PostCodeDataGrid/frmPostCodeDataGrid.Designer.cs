c1e239c0cf29ac2082eabc5322188bb4a815727f
tjtik83@gmail.com <tjtik83@gmail.com>
2021-07-20 14:02:23 +0700

    completed

---
+++ PostCodeDataGrid/frmPostCodeDataGrid.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace PostCodeDataGrid
+{
+    partial class frmPostCodeDataGrid
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnAddRow = new System.Windows.Forms.Button();
+            this.btnRemoveROw = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 373);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
+            // 
+            // btnAddRow
+            // 
+            this.btnAddRow.Location = new System.Drawing.Point(586, 401);
+            this.btnAddRow.Name = "btnAddRow";
+            this.btnAddRow.Size = new System.Drawing.Size(98, 37);
+            this.btnAddRow.TabIndex = 1;
+            this.btnAddRow.Text = "Add Row";
+            this.btnAddRow.UseVisualStyleBackColor = true;
+            this.btnAddRow.Click += new System.EventHandler(this.btnAddRow_Click);
+            // 
+            // btnRemoveROw
+            // 
+            this.btnRemoveROw.Location = new System.Drawing.Point(690, 401);
+            this.btnRemoveROw.Name = "btnRemoveROw";
+            this.btnRemoveROw.Size = new System.Drawing.Size(98, 37);
+            this.btnRemoveROw.TabIndex = 1;
+            this.btnRemoveROw.Text = "Remove Row";
+            this.btnRemoveROw.UseVisualStyleBackColor = true;
+            this.btnRemoveROw.Click += new System.EventHandler(this.btnRemoveROw_Click);
+            // 
+            // frmPostCodeDataGrid
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRemoveROw);
+            this.Controls.Add(this.btnAddRow);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "frmPostCodeDataGrid";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Thai Postcode thanks to Thongkorn Tubtimkrob via Github";
+            this.Load += new System.EventHandler(this.frmPostCodeDataGrid_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnAddRow;
+        private System.Windows.Forms.Button btnRemoveROw;
+    }
+}
+
---
+++ PostCodeDataGrid/frmPostCodeDataGrid.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PostCodeDataGrid
+{
+    public partial class frmPostCodeDataGrid : Form
+    {
+        // nugat system.data.oledb
+        private OleDbConnection Conn;
+
+        private OleDbCommand Cmd;
+        private OleDbDataReader DR;
+        private OleDbDataAdapter DA;
+        private string sql = string.Empty;
+        private DataGridViewComboBoxColumn ColProvice = new DataGridViewComboBoxColumn();
+        private DataGridViewComboBoxColumn ColAmphur = new DataGridViewComboBoxColumn();
+        private DataGridViewComboBoxColumn ColTumbon = new DataGridViewComboBoxColumn();
+        private DataGridViewTextBoxColumn ColPostCode = new DataGridViewTextBoxColumn();
+
+        public frmPostCodeDataGrid()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPostCodeDataGrid_Load(object sender, EventArgs e)
+        {
+            // กำหนด Path ของไฟล์ฐานข้อมูล
+            string strPath = Application.StartupPath;
+            strPath = strPath.ToLower();
+            //strPath = strPath.Replace(@"\net5.0-windows\", "");
+            strPath = strPath.Replace(@"\bin\debug", @"\");
+
+            string strConn = string.Format($"Provider=Microsoft.Jet.OLEDB.4.0;Data Source ={strPath}Data\\PostCode.mdb;Persist Security Info=False;");
+
+            try
+            {
+                // เปิดการเชื่อมต่อไฟล์ฐานข้อมูล
+                Conn = new OleDbConnection(strConn);
+                Conn.Open();
+                dataGridView1.AllowUserToAddRows = false;
+                dataGridView1.AllowUserToDeleteRows = false;
+                dataGridView1.SelectionMode = DataGridViewSelectionMode.CellSelect;
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                dataGridView1.MultiSelect = false;
+
+                // จัดการ จังหวัด
+                ColProvice.Name = "Province";
+                ColProvice.HeaderText = "จังหวัด";
+                ColProvice.ReadOnly = false;
+                ColProvice.MaxDropDownItems = 10;
+                dataGridView1.Columns.Add(ColProvice);
+                LoadProvince();
+
+                // จัดการ อำเภอ
+                ColAmphur.Name = "Amphur";
+                ColAmphur.HeaderText = "เขต/อำเภอ";
+                ColAmphur.ReadOnly = false;
+                ColAmphur.MaxDropDownItems = 10;
+                dataGridView1.Columns.Add(ColAmphur);
+
+                // จัดการ ตำบล
+                ColTumbon.Name = "Tumbon";
+                ColTumbon.HeaderText = "ตำบล";
+                ColTumbon.ReadOnly = false;
+                ColTumbon.MaxDropDownItems = 10;
+                dataGridView1.Columns.Add(ColTumbon);
+
+                // จัดการ รหัสไปรษณีย์
+                ColPostCode.Name = "PostCode";
+                ColPostCode.HeaderText = "รหัสไปรษณีย์";
+                ColPostCode.ReadOnly = false;
+                dataGridView1.Columns.Add(ColPostCode);
+
+                // Add New Row
+                dataGridView1.Rows.Add("", "", "", "");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Application.Exit();
+            }
+        }
+
+        /// <summary>
+        /// โหลดรายชื่อจังหวัดเข้าสู่ ComboBox
+        /// </summary>
+        private void LoadProvince()
+        {
+            // DISTINCT คือ หากชื่อรายการมันซ้ำ ต้องตัดให้เหลือเพียงรายการเดียว
+            sql = "SELECT DISTINCT PostCode.Province From PostCode ORDER BY PostCode.Province";
+            Cmd = new OleDbCommand(sql, Conn);
+            DR = Cmd.ExecuteReader();
+            while (DR.Read())
+            {
+                ColProvice.Items.Add(DR["Province"].ToString());
+            }
+            Conn.Close();
+        }
+
+        /// <summary>
+        /// โหลดรายชื่ออำเภอ หลังจากเลือกจังหวัด
+        /// </summary>
+        /// <param name="Province"></param>
+        /// <param name="row"></param>
+        private void LoadAmphur(string Province, int row)
+        {
+            sql = string.Format(@"SELECT DISTINCT PostCode.Amphur, PostCode.Province From PostCode WHERE Province = '{0}' ORDER BY PostCode.Amphur", Province);
+
+            try
+            {
+                DataGridViewRow gridRow = dataGridView1.Rows[row];
+                DataGridViewComboBoxCell MyCell = new DataGridViewComboBoxCell();
+                MyCell = (DataGridViewComboBoxCell)gridRow.Cells["Amphur"];
+                if (Conn.State == ConnectionState.Closed) Conn.Open();
+                DA = new OleDbDataAdapter(sql, Conn);
+                DataTable dt = new DataTable();
+                DA.Fill(dt);
+                MyCell.Items.Clear();
+                foreach (DataRow dataRow in dt.Rows)
+                {
+                    MyCell.Items.Add(dataRow["Amphur"].ToString());
+                }
+                dt.Dispose();
+                DA.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// โหลดรายชื่อตำบล หลังจากเลือกจังหวัดและอำเภอ
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void LoadTumbon(string Province, string Amphur, int row)
+        {
+            sql = string.Format(@"SELECT PostCode.Amphur, PostCode.Province, PostCode.Tumbon From PostCode WHERE Province = '{0}' AND Amphur = '{1}' ORDER BY PostCode.Tumbon", Province, Amphur);
+
+            try
+            {
+                DataGridViewRow gridRow = dataGridView1.Rows[row];
+                DataGridViewComboBoxCell MyCell = new DataGridViewComboBoxCell();
+                MyCell = (DataGridViewComboBoxCell)gridRow.Cells["Tumbon"];
+                if (Conn.State == ConnectionState.Closed) Conn.Open();
+                DA = new OleDbDataAdapter(sql, Conn);
+                DataTable dt = new DataTable();
+                DA.Fill(dt);
+                MyCell.Items.Clear();
+                foreach (DataRow dataRow in dt.Rows)
+                {
+                    MyCell.Items.Add(dataRow["Tumbon"].ToString());
+                }
+                dt.Dispose();
+                DA.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// สุดท้าย โหลด รหัสไปรษณีย์
+        /// </summary>
+        /// <param name="province"></param>
+        /// <param name="amphur"></param>
+        /// <param name="tumbon"></param>
+        /// <param name="rowIndex"></param>
+        private void LoadPostCode(string province, string amphur, string tumbon, int rowIndex)
+        {
+            sql = string.Format(@"SELECT PostCode.Province, PostCode.Amphur, PostCode.Tumbon, PostCode.PostCode, PostCode.Remark From PostCode WHERE Province = '{0}' AND Amphur = '{1}' AND Tumbon = '{2}' ORDER BY PostCode.Tumbon", province, amphur, tumbon);
+
+            try
+            {
+                if (Conn.State == ConnectionState.Closed) Conn.Open();
+                Cmd = new OleDbCommand(sql, Conn);
+                DR = Cmd.ExecuteReader();
+                while (DR.Read())
+                {
+                    dataGridView1.Rows[rowIndex].Cells[3].Value = DR["PostCode"].ToString();
+                }
+                DR.Close();
+                Cmd.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            string Province = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+            string Amphur = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            string Tumbon = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+
+            switch (e.ColumnIndex)
+            {
+                case 0:
+                    //dataGridView1[1, e.RowIndex].Value.ToString();
+                    LoadAmphur(Province, e.RowIndex);
+                    break;
+
+                case 1:
+                    if (Province != "" && Amphur != "")
+                    {
+                        LoadTumbon(Province, Amphur, e.RowIndex);
+                    }
+                    break;
+
+                case 2:
+                    if (Province != "" && Amphur != "" && Tumbon != "")
+                    {
+                        LoadPostCode(Province, Amphur, Tumbon, e.RowIndex);
+                    }
+                    break;
+            }
+        }
+
+        private void btnAddRow_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Add("", "", "", "");
+        }
+
+        private void btnRemoveROw_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.RowCount == 0)
+            {
+                return;
+            }
+            else
+            {
+                dataGridView1.Rows.Remove(dataGridView1.CurrentRow);
+            }
+        }
+    }
+}
